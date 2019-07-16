0321f17af189d719193b26db984d643633646da7
Sairam Kammili <sairamkammili@gmail.com>
2019-07-15 18:56:17 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+import React,{ useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+  function Square({numbers,onClick}){
+    return (
+        <button className="square" onClick={onClick}>
+            {numbers}
+        </button>
+      );
+  }
+  
+  function Board(){
+      const [X, XNext] = useState(Array(9).fill(null))
+      const [lastTurn, setLastTurn] = useState(true)
+        function renderSquare(i) {
+        return <Square numbers = {X[i]}
+        onClick ={()=>{
+            const square = X.slice()
+            square[i] = lastTurn ? 'X' :'O'
+            setLastTurn(!lastTurn)
+            XNext(square)
+        }
+        
+        }
+        />;
+        }
+    
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
